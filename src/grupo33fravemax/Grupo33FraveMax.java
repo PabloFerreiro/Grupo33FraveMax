b01e183daf029fc302060cb5c15e32ccd22789a0
VmaOranSalta2023 <vmasistemas@hotmail.com>
2023-10-03 15:27:04 -0300

    actualizado 03/10/23 hs: 15:26

--- src/grupo33fravemax/Grupo33FraveMax.java
+++ src/grupo33fravemax/Grupo33FraveMax.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+// Actualizado dia: 03/10/23 hs: 15:26
 package grupo33fravemax;
 
 import accesoADatos.ClienteData;
